783164d259bb6d93c654773747ad15a77e63b7c2
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-05 22:28:04 +0100

    Banque terminé

--- index.php
+++ index.php
@@ -16,13 +16,19 @@ $titulaireOne = new Titulaire("Ansu", "Fati", '1993-11-15', "STRASBOURG");
 $banqueCompte = new Comptebanque("CompteCourant", 100.5, 'EUR', $titulaireOne);
 $LivretA = new ComptelivretA("Livret A", 500.0, "EUR", $titulaireOne);
 
+//Ajout des compte pour un titulaire
 $titulaireOne->ajouterCompte($banqueCompte);
 $titulaireOne->ajouterCompte($LivretA);
+//Affiche Info
 $titulaireOne->afficherInformations();
 
 
 $banqueCompte->afficherInfo();
-$LivretA->crediter(50.0);
-
+$banqueCompte->crediter(50.0);
+//Pour effectuer un virement
+echo "<br>************************************************<br>";
 $LivretA->virementVersUnAutreCompte($banqueCompte, 30);
 $banqueCompte->afficherInfo();
+
+echo "<br>************************************************<br>";
+$LivretA->afficherInfo();
